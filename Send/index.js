70293a48e9eee7a4e1caa089e3afc624f2f47da4
vignesh <vigneshfornavy@gmail.com>
2023-10-30 11:41:09 +0530

    export and require

---
+++ Send/index.js
@@ -0,0 +1,9 @@
+
+let first = require("./apple");
+let second = require("./orange");
+
+
+let fruits = [first,second];
+
+
+module.exports = fruits[0].color;
--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// let a = 10;
-// let b = 5;
+let info = require("./Send");
 
-
-// console.log(a+b);
-
-
-console.log(process.argv);
-
-let args = process.argv;
-for(let i = 2;i<=args.length;i++){
-    console.log(`value is ${i} and ${args[i]}`)
-}
+console.log(info);
